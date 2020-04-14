adace108dcb3a6fd2c1ee1390b2f64d12e5ac303
Josué Clément <mod6991@gmail.com>
2020-04-14 17:09:01 +0200

    Start DbLayerItem

--- DotNetToolBox.DbManagerCodeGenerator/DbmCodeGenerator.cs
+++ DotNetToolBox.DbManagerCodeGenerator/DbmCodeGenerator.cs
@@ -99,7 +99,109 @@ namespace DotNetToolBox.DbManagerCodeGenerator
 
         private void GenerateDbLayerItems()
         {
+            foreach(DbItem dbi in _dbItems)
+            {
+                string file = Path.Combine(_outputPath, $"{_dbLayerObjectName}.{dbi.ObjectName}.cs");
+                string minObjName = char.ToLower(dbi.ObjectName[0]) + dbi.ObjectName.Substring(1);
 
+                using (FileStream fs = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs, _encoding))
+                    {
+                        bool first = true;
+
+                        sw.WriteLine($"using {_objectsNamespace};");
+                        sw.WriteLine("using System;");
+                        sw.WriteLine("using System.Collections.Generic;");
+                        sw.WriteLine("using System.Data.Common;");
+                        sw.WriteLine();
+                        sw.WriteLine($"namespace {_dbLayerNamespace}");
+                        sw.WriteLine("{"); //start namespace
+                        sw.WriteLine($"{WriteIndentCs(1)}public partial class {_dbLayerObjectName}");
+                        sw.WriteLine($"{WriteIndentCs(1)}{{"); //start class
+
+                        if (dbi.UseSelectAll)
+                        {
+                            sw.WriteLine($"{WriteIndentCs(2)}public List<{dbi.ObjectName}> SelectAll{dbi.ObjectName}s()");
+                            sw.WriteLine($"{WriteIndentCs(2)}{{"); //start SelectAll
+                            sw.WriteLine($"{WriteIndentCs(3)}List<{dbi.ObjectName}> list = _db[\"{dbi.ObjectName}\"].FillObjects<{dbi.ObjectName}>(\"SelectAll{dbi.ObjectName}s\", null);");
+                            sw.WriteLine($"{WriteIndentCs(3)}return list;");
+                            sw.WriteLine($"{WriteIndentCs(2)}}}"); //end SelectAll
+                            first = false;
+                        }
+
+                        if (dbi.UseSelectById)
+                        {
+                            if (!first)
+                                sw.WriteLine();
+
+                            sw.WriteLine($"{WriteIndentCs(2)}public {dbi.ObjectName} Select{dbi.ObjectName}ById({dbi.Fields[0].DataType} {dbi.Fields[0].ParameterName})");
+                            sw.WriteLine($"{WriteIndentCs(2)}{{"); //start SelectById
+                            sw.WriteLine($"{WriteIndentCs(3)}List<DbParameter> parameters = new List<DbParameter>();");
+                            sw.WriteLine($"{WriteIndentCs(3)}parameters.Add(_db.CreateParameter(\"{dbi.Fields[0].ParameterName}\", {dbi.Fields[0].ParameterName}));");
+                            sw.WriteLine($"{WriteIndentCs(3)}List<{dbi.ObjectName}> list = _db[\"{dbi.ObjectName}\"].FillObjects<{dbi.ObjectName}>(\"Select{dbi.ObjectName}ById\", parameters);");
+                            sw.WriteLine($"{WriteIndentCs(3)}if (list.Count == 0)");
+                            sw.WriteLine($"{WriteIndentCs(4)}return null;");
+                            sw.WriteLine($"{WriteIndentCs(3)}return list[0];");
+                            sw.WriteLine($"{WriteIndentCs(2)}}}"); //end SelectById
+                            first = false;
+                        }
+
+                        if (dbi.UseInsert)
+                        {
+                            if (!first)
+                                sw.WriteLine();
+
+                            sw.WriteLine($"{WriteIndentCs(2)}public void Insert{dbi.ObjectName}({dbi.ObjectName} {minObjName})");
+                            sw.WriteLine($"{WriteIndentCs(2)}{{"); //start Insert
+                            sw.WriteLine($"{WriteIndentCs(3)}List<DbParameter> parameters = new List<DbParameter>();");
+
+                            foreach (DbField field in dbi.Fields)
+                                sw.WriteLine($"{WriteIndentCs(3)}parameters.Add(_db.CreateParameter(\"{field.ParameterName}\", {minObjName}.{field.PropertyName}));");
+
+                            sw.WriteLine($"{WriteIndentCs(3)}_db[\"{dbi.ObjectName}\"].ExecuteNonQuery(\"Insert{dbi.ObjectName}\", parameters);");
+                            sw.WriteLine($"{WriteIndentCs(2)}}}"); //end Insert
+                            first = false;
+                        }
+
+                        if (dbi.UseUpdateSingle)
+                        {
+                            if (!first)
+                                sw.WriteLine();
+
+                            sw.WriteLine($"{WriteIndentCs(2)}public void Update{dbi.ObjectName}({dbi.ObjectName} {minObjName})");
+                            sw.WriteLine($"{WriteIndentCs(2)}{{"); //start Insert
+                            sw.WriteLine($"{WriteIndentCs(3)}List<DbParameter> parameters = new List<DbParameter>();");
+
+                            foreach (DbField field in dbi.Fields)
+                                sw.WriteLine($"{WriteIndentCs(3)}parameters.Add(_db.CreateParameter(\"{field.ParameterName}\", {minObjName}.{field.PropertyName}));");
+
+                            sw.WriteLine($"{WriteIndentCs(3)}_db[\"{dbi.ObjectName}\"].ExecuteNonQuery(\"Insert{dbi.ObjectName}\", parameters);");
+                            sw.WriteLine($"{WriteIndentCs(2)}}}"); //end Insert
+                            first = false;
+                        }
+
+                        if (dbi.UseUpdateAll)
+                        {
+                            if (!first)
+                                sw.WriteLine();
+
+
+                        }
+
+                        if (dbi.UseDelete)
+                        {
+                            if (!first)
+                                sw.WriteLine();
+
+
+                        }
+
+                        sw.WriteLine($"{WriteIndentCs(1)}}}"); //end class
+                        sw.WriteLine("}"); //end namespace
+                    }
+                }
+            }
         }
 
         private void GenerateQueries()
